f73b2a54a1d6ead2f1f3e830f1952cad74f95ab5
sorijoo <48428325+sorijoo@users.noreply.github.com>
2022-07-17 14:32:32 +0900

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -15,15 +15,15 @@ import pandas as pd
 from skimage.io import imread
 from skimage.transform import resize
 from PIL import Image
-# from imgaug import augmenters as iaa
+from imgaug import augmenters as iaa
 
 from sklearn.model_selection import train_test_split
 
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
-from keras.utils import *
-from keras.callbacks import *
+# from keras.models import *
+# from keras.layers import *
+# from keras.optimizers import *
+# from keras.utils import *
+# from keras.callbacks import *
 
 from keras.applications.densenet import DenseNet121, preprocess_input
 
