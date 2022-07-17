84dafe6e4dcc678c325ad49b5c5dbbaa8e9c7df0
sorijoo <48428325+sorijoo@users.noreply.github.com>
2022-07-17 15:41:58 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown('# CV Mini Project')
+st.markdown(
+    '## [강아지 분류 예측 모델](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
+st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
+
+
+
+st.markdown('---')
+
+st.markdown("팀장: 주소리")
+st.markdown("팀원: 이주승, 김재석")
