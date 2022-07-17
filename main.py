e0c5378ea58b96db7b725d62088bae5fdfe0c797
sorijoo <sorij95@gmail.com>
2022-07-17 17:07:49 +0900

    갱얼

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.markdown(
     '## [강아지 분류 예측 모델](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
 st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
 
-st.markdown("![Alt Text](https://img.extmovie.com/files/attach/images/148/940/183/064/1581897c5141219ee143ebbdd6ca31a6.gif)")
+st.markdown("![Alt Text](https://media.giphy.com/media/kxlDaFDVzTLb2/giphy.gif)")
 
 st.markdown('---')
 
